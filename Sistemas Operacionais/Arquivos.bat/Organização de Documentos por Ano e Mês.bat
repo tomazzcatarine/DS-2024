d9b6a8814354248970e3daaf0a5ddfed436b7774
Catarine Tomaz <165063607+tomazzcatarine@users.noreply.github.com>
2024-04-09 11:49:00 -0300

    Add files via upload

---
+++ Sistemas Operacionais/Arquivos.bat/Organização de Documentos por Ano e Mês.bat
@@ -0,0 +1,20 @@
+@echo off
+::BIBLIOTECA (DIC.) UTF-8
+chcp 65001 >nul 
+
+set/p ano="Digite um ano"
+mkdir %ano%
+cd %ano%
+
+mkdir="1-Janeiro"
+mkdir="2-Fevereiro"
+mkdir="3-Março"
+mkdir="4-Abril"
+mkdir="5-Maio"
+mkdir="6-Junho"
+mkdir="7-Julho"
+mkdir="8-Agosto"
+mkdir="9-Setembro"
+mkdir="10-Outubro"
+mkdir="11-Novembro"
+mkdir="12-Dezembro"
